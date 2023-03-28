2cbcffa60bf358167a9f8018798df2d1ba2bd448
Prabodashan <prabodashan14@gmail.com>
2023-03-28 14:11:58 +0530

    Added Query Parameters

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get('/')
-def index():
-    return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit=10, published:bool=True, sort: Optional[str]=None):
+    # only get 10 published blogs
+    if published:
+        return {'data': f'{limit} published blog from the'}
+    else:
+        return {'data': f'{limit} blog from the'}
+
 
 @app.get('/blog/unpublished')
 def unpublished():
@@ -16,6 +22,6 @@ def show(id:int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     #fetch comments of blog with id = id
     return{'data': {'1', '2'}}

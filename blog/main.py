b4022af89215bb08b234cca3144ec8cf34521a0d
Prabodashan <prabodashan14@gmail.com>
2023-03-28 16:16:24 +0530

    update and delete blog

--- blog/main.py
+++ blog/main.py
@@ -22,6 +22,22 @@ def create(request:schemas.Blog, db: Session = Depends(get_db)):
    db.refresh(new_blog)
    return new_blog
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return "blog is deleted"
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request:schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail= f"Blog with the id {id} is not available")
+    blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'Updated successfully'
+
+
 
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):

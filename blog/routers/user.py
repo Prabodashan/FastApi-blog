febdc928c99dfa9914ccd07950372ce2c587ed44
Prabodashan <prabodashan14@gmail.com>
2023-03-28 19:29:59 +0530

    added Blog & User respository

--- blog/routers/user.py
+++ blog/routers/user.py
@@ -3,20 +3,17 @@ from .. import schemas, models, database
 from sqlalchemy.orm import Session
 from ..hashing import Hash
 
-router = APIRouter()
+from ..repository import user
+
+router = APIRouter(    
+    prefix="/user",
+    tags=['Users'])
 get_db = database.get_db
 
-@router.post('/user', response_model=schemas.ShowUser, tags=['users'])
+@router.post('/', response_model=schemas.ShowUser)
 def create_user(request:schemas.User, db: Session = Depends(get_db)):
-    new_User = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_User)
-    db.commit()
-    db.refresh(new_User)
-    return new_User
+    return user.create(request, db)
 
-@router.get('/user/{id}',response_model=schemas.ShowUser, tags=['users'])
+@router.get('/{id}',response_model=schemas.ShowUser)
 def show(id:int,db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with the id {id} is not available")
-    return user
+    return user.show()
